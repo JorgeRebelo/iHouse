ba57cd5babc1d6ca423ca8336db58eb13e4d1d1f
Jorge Rebelo <jorge.rebelo.ae@gmail.com>
2017-11-07 23:21:56 +0000

    Created Classes Server and Client

---
+++ src/org/academiadecodigo/enuminatti/ihouse/client/Client.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.enuminatti.ihouse.client;
+
+/**
+ * Created by codecadet on 07/11/17.
+ */
+public class Client {
+
+}
---
+++ src/org/academiadecodigo/enuminatti/ihouse/server/Server.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.enuminatti.ihouse.server;
+
+/**
+ * Created by codecadet on 07/11/17.
+ */
+public class Server {
+}
