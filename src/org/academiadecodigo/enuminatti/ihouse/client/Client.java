649dac56308ee6a55c8c3d43fe93de3a1a1454e0
Drakendor <drake-777@hotmail.com>
2017-11-08 19:50:57 +0000

    Created Server accepting clients

--- src/org/academiadecodigo/enuminatti/ihouse/client/Client.java
+++ src/org/academiadecodigo/enuminatti/ihouse/client/Client.java
@@ -1,8 +1,23 @@
 package org.academiadecodigo.enuminatti.ihouse.client;
 
+import java.net.Socket;
+
 /**
  * Created by codecadet on 07/11/17.
  */
 public class Client {
+
+    private Socket socket;
+
+
+
+    void connect(){
+
+    }
+
+
+    void disconnect(){
+
+    }
 
 }
--- src/org/academiadecodigo/enuminatti/ihouse/server/Server.java
+++ src/org/academiadecodigo/enuminatti/ihouse/server/Server.java
@@ -1,7 +1,80 @@
 package org.academiadecodigo.enuminatti.ihouse.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by codecadet on 07/11/17.
  */
 public class Server {
+
+
+    private ExecutorService threadPool;
+    private LinkedList<ServerWorker> workerList;
+
+    public Server() {
+
+    }
+
+    public static void main(String[] args) {
+
+
+        Server server = new Server();
+        server.threadPool = Executors.newCachedThreadPool();
+        server.workerList = new LinkedList<>();
+
+        ServerSocket svSocket;
+
+        try {
+            //We need to accept with various IPs
+            svSocket = new ServerSocket(8080);
+            server.start(svSocket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+
+    public void start(ServerSocket socket){
+
+        while(true){
+            try {
+                Socket clientSocket = socket.accept();
+                ServerWorker svWorker = new ServerWorker(clientSocket);
+                threadPool.submit(svWorker);
+                workerList.add(svWorker);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+
+
+
+    public class ServerWorker implements Runnable {
+
+        Socket clientSocket;
+
+        public ServerWorker(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
 }
