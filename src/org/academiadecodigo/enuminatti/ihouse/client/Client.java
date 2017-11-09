64c496721c3850496a24b642f2988588f78cd091
LuisftSilva <ftsluis@gmail.com>
2017-11-09 21:34:43 +0000

    client fixed

--- src/org/academiadecodigo/enuminatti/ihouse/client/Client.java
+++ src/org/academiadecodigo/enuminatti/ihouse/client/Client.java
@@ -2,110 +2,90 @@ package org.academiadecodigo.enuminatti.ihouse.client;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
+
 
 /**
- * Created by codecadet on 07/11/17.
+ * Created by codecadet on 31/10/17.
  */
-public class Client {
+public class Client implements Runnable {
+
+    Socket clientSocket;
+    Thread thread;
+    boolean keepRead = true;
+
+    public Client() {
+
+        try {
+            clientSocket = new Socket("localhost", 8080);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String sentence;
+
+        sentence = scanner.nextLine();
+
+        BufferedWriter outToServer = null;
+        try {
+            outToServer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            outToServer.write(sentence);
+            outToServer.newLine();
+            outToServer.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void read() {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String sentence = bufferedReader.readLine();
+
+            if (sentence == null) {
+
+                clientSocket.close();
+                thread.interrupt();
+                keepRead = false;
+
+            } else {
+
+                System.out.println(sentence);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        try {
-            Socket clientSocket = new Socket("localhost", 8080);
-            SendThread sendThread = new SendThread(clientSocket);
-            Thread thread1 = new Thread(sendThread);
-            thread1.start();
-            ReceiveThread receiveThread = new ReceiveThread(clientSocket);
-            Thread thread2 = new Thread(receiveThread);
-            thread2.start();
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        Client client = new Client();
+
+        client.thread = new Thread(client);
+        client.thread.start();
+
+        while (true) {
+
+            client.write();
         }
     }
-
-    //Socket clientSocket = new Socket("localhost", 8080);
-    //SendThread sendThread = new SendThread(clientSocket);
-
-
-}
-
-class SendThread implements Runnable {
-
-    Socket socket;
-    BufferedReader reader;
-    PrintWriter writer;
-
-    public SendThread(Socket socket) {
-        this.socket = socket;
-    }
-
 
     @Override
     public void run() {
 
-
-        try {
-
-            while (true) {
-
-                reader = new BufferedReader(new InputStreamReader(System.in));
-                writer = new PrintWriter(socket.getOutputStream());
-
-                String command;
-
-                while((command = reader.readLine()) != null){
-                    System.out.println("Client: " + command);
-                    writer.println(command);
-                }
-            }
-
-        } catch (IOException e) {
-
-            System.out.println("Send thread failed " + e.getMessage());
-        }
-    }
-
-
-    public void tryClose() {
-        try {
-            if (writer != null) {
-                writer.close();
-            }
-            if (reader != null) {
-                reader.close();
-            }
-            if (socket != null) {
-                socket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (keepRead) {
+            read();
         }
     }
 }
-
-class ReceiveThread implements Runnable {
-
-    Socket socket;
-    BufferedReader reader;
-
-    public ReceiveThread(Socket socket) {
-        this.socket = socket;
-    }
-
-    @Override
-    public void run() {
-        try {
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String command;
-
-            while ((command = reader.readLine()) != null) {
-                System.out.println(command);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
