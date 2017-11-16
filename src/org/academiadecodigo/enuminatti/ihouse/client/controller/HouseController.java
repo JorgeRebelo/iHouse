e5c14bf344e727f93aa8afc67ace45bc99848b7f
Drakendor <drake-777@hotmail.com>
2017-11-16 18:01:23 +0000

    Accidentally removed files in last commit

---
+++ src/org/academiadecodigo/enuminatti/ihouse/client/controller/HouseController.java
@@ -0,0 +1,277 @@
+package org.academiadecodigo.enuminatti.ihouse.client.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.academiadecodigo.enuminatti.ihouse.client.Client;
+import org.academiadecodigo.enuminatti.ihouse.client.utils.Navigation;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by codecadet on 08/11/17.
+ */
+public class HouseController implements Controller {
+
+    private String houseStatus;
+    private List<Button> elements;
+    private Client client;
+
+
+    @FXML
+    private Button masterBedroomLightButton;
+
+    @FXML
+    private Button bedroomLightButton;
+
+    @FXML
+    private Button livingroomLightButton;
+
+    @FXML
+    private Button kitchenLightButton;
+
+    @FXML
+    private Button bathroomLightButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button masterBedroomBlind;
+
+    @FXML
+    private Button kitchenBlind;
+
+    @FXML
+    private Button livingroomBlind;
+
+    @FXML
+    private Button bedroomBlind;
+
+
+    //----------LIGHT BUTTON METHODS----------//
+
+    @FXML
+    void onBathroomLightButton(ActionEvent event) {
+
+        updateLights(bathroomLightButton, bathroomLightButton.getText());
+        //bathroomLightButton.setStyle("-fx-background-radius: 5em");
+        client.write(getHouseStatus());
+
+    }
+
+    @FXML
+    void onBedroomLightButton(ActionEvent event) {
+
+        updateLights(bedroomLightButton, bedroomLightButton.getText());
+        //bedroomLightButton.setStyle("-fx-background-radius: 5em");
+        client.write(getHouseStatus());
+
+    }
+
+    @FXML
+    void onKitchenLightButton(ActionEvent event) {
+
+        updateLights(kitchenLightButton, kitchenLightButton.getText());
+        //kitchenLightButton.setStyle("-fx-background-radius: 5em");
+        client.write(getHouseStatus());
+
+    }
+
+    @FXML
+    void onLivingroomLightButton(ActionEvent event) {
+
+        updateLights(livingroomLightButton, livingroomLightButton.getText());
+        //livingroomLightButton.setStyle("-fx-background-radius: 5em");
+        client.write(getHouseStatus());
+
+    }
+
+    @FXML
+    void onMasterBedroomLightButton(ActionEvent event) {
+
+        updateLights(masterBedroomLightButton, masterBedroomLightButton.getText());
+        //masterBedroomLightButton.setStyle("-fx-background-radius: 5em");
+        client.write(getHouseStatus());
+
+    }
+
+    @FXML
+    void onLogoutButton(ActionEvent event) {
+
+        Navigation.getInstance().back();
+    }
+
+    @FXML
+    void onExitButton(ActionEvent event) {
+        client.disconnect();
+        //Close all communications opened*******************************************************************************
+        Platform.exit();
+    }
+    //---------BLIND BUTTON METHODS----------//
+
+    @FXML
+    void onBedroomBlind(ActionEvent event) {
+        updateBlinds(bedroomBlind, bedroomBlind.getText());
+        client.write(getHouseStatus());
+    }
+
+    @FXML
+    void onKitchenBlind(ActionEvent event) {
+        updateBlinds(kitchenBlind, kitchenBlind.getText());
+        client.write(getHouseStatus());
+
+    }
+
+    @FXML
+    void onLivingroomBlind(ActionEvent event) {
+        updateBlinds(livingroomBlind, livingroomBlind.getText());
+        client.write(getHouseStatus());
+    }
+
+    @FXML
+    void onMasterBedroomBlind(ActionEvent event) {
+        updateBlinds(masterBedroomBlind, masterBedroomBlind.getText());
+        client.write(getHouseStatus());
+    }
+
+
+    //---------CONTROLLER METHODS----------//
+
+    @FXML
+    void initialize() {
+        assert masterBedroomLightButton != null : "fx:id=\"masterBedroomLightButton\" was not injected: check your FXML file 'house.fxml'.";
+        assert bedroomLightButton != null : "fx:id=\"bedroomLightButton\" was not injected: check your FXML file 'house.fxml'.";
+        assert livingroomLightButton != null : "fx:id=\"livingroomLightButton\" was not injected: check your FXML file 'house.fxml'.";
+        assert kitchenLightButton != null : "fx:id=\"kitchenLightButton\" was not injected: check your FXML file 'house.fxml'.";
+        assert bathroomLightButton != null : "fx:id=\"bathroomLightButton\" was not injected: check your FXML file 'house.fxml'.";
+        assert exitButton != null : "fx:id=\"exitButton\" was not injected: check your FXML file 'house.fxml'.";
+        assert logOutButton != null : "fx:id=\"logOutButton\" was not injected: check your FXML file 'house.fxml'.";
+        assert masterBedroomBlind != null : "fx:id=\"masterBedroomBlind\" was not injected: check your FXML file 'house.fxml'.";
+        assert kitchenBlind != null : "fx:id=\"kitchenBlind\" was not injected: check your FXML file 'house.fxml'.";
+        assert livingroomBlind != null : "fx:id=\"livingroomBlind\" was not injected: check your FXML file 'house.fxml'.";
+        assert bedroomBlind != null : "fx:id=\"bedroomBlind\" was not injected: check your FXML file 'house.fxml'.";
+
+        elements = new LinkedList<>();
+        elements.add(masterBedroomLightButton);
+        elements.add(bedroomLightButton);
+        elements.add(livingroomLightButton);
+        elements.add(kitchenLightButton);
+        elements.add(bathroomLightButton);
+        elements.add(masterBedroomBlind);
+        elements.add(kitchenBlind);
+        elements.add(livingroomBlind);
+        elements.add(bedroomBlind);
+
+    }
+
+    //Get command from server
+    public void getCommand(String serverCommand) {
+
+        System.out.println("getCommand() called");
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("Platform Runlater: " + Thread.currentThread().getName());
+
+                String[] element = serverCommand.split("/");
+                System.out.println("getCommand() executing command: " + serverCommand);
+
+                for (int i = 0; i < element.length; i++) {
+
+                    String[] status = element[i].split("=");
+                    System.out.println("Element :  " + status[0] + " -> " + status[1] + " ");
+
+                    //this is not good yet..
+                    switch (status[0]) {
+                        case "masterBedroomLightButton":
+                            updateLights(masterBedroomLightButton, status[1]);
+                            break;
+                        case "livingroomLightButton":
+                            updateLights(livingroomLightButton, status[1]);
+                            break;
+                        case "bedroomLightButton":
+                            updateLights(bedroomLightButton, status[1]);
+                            break;
+                        case "bathroomLightButton":
+                            updateLights(bathroomLightButton, status[1]);
+                            break;
+                        case "kitchenLightButton":
+                            updateLights(kitchenLightButton, status[1]);
+                            break;
+                        case "livingroomBlind":
+                            updateBlinds(livingroomBlind, status[1]);
+                            break;
+                        case "kitchenBlind":
+                            updateBlinds(kitchenBlind, status[1]);
+                            break;
+                        case "masterBedroomBlind":
+                            updateBlinds(masterBedroomBlind, status[1]);
+                            break;
+                        case "bedroomBlind":
+                            updateBlinds(bedroomBlind, status[1]);
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+    //Update the lights
+    private void updateLights(Button button, String status) {
+        if (status.equals("1") || status.equals("OFF")) {
+            button.setText("ON");
+            button.setStyle("-fx-background-color: yellow");
+            //button.setStyle("-fx-background-radius: 10em");
+            return;
+        }
+        button.setText("OFF");
+        button.setStyle("-fx-background-color: lightgray");
+    }
+
+    //Update the blinds
+    private void updateBlinds(Button button, String status) {
+        if (status.equals("1") || status.equals("CLOSE")) {
+            button.setText("OPEN");
+            button.setStyle("-fx-background-color: deepskyblue");
+            return;
+        }
+        button.setText("CLOSE");
+        button.setStyle("-fx-background-color: black");
+    }
+
+    //Get message to send to server
+    private String getHouseStatus() {
+
+        houseStatus = "";
+
+        for (Button element : elements) {
+            String elementStatus;
+
+            //for every iteration, know how the light is right now
+            if (element.getText().equals("OFF") || element.getText().equals("CLOSE")) {
+                elementStatus = "0";
+            } else {
+                elementStatus = "1";
+            }
+
+            houseStatus += element.getId() + "=" + elementStatus + "/";
+
+        }
+
+        System.out.println("Client house updated!");
+        System.out.println("-----------------------" + "\n");
+        return houseStatus;
+    }
+
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
